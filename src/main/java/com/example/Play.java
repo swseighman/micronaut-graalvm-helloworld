171f10f9d183fb8b77263b0fca694540cd751eb5
swseighman <scottseighman@gmail.com>
2021-01-22 15:56:35 -0500

    Initial commit

---
+++ src/main/java/com/example/Play.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import io.micronaut.core.annotation.Introspected;
+
+@Introspected
+public class Play{
+
+    private String name;
+
+    public Play(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/PlayService.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import javax.inject.Singleton;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@Singleton
+public class PlayService {
+    // create list of plays
+    private static final List<Play> PLAYS = Arrays.asList(
+            new Play("Hello GraalVM Workshop!"),
+            new Play("Happy New Year!"),
+            new Play("Micronaut & GraalVM"),
+            new Play("Java Rules!"),
+            new Play("Maven or Gradle?")
+    );
+    // to choose random play from PLAYS list
+    public Play randomPlay() {
+        return PLAYS.get(new Random().nextInt(PLAYS.size()));
+    }
+}
